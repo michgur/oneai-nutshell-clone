27787a406430e5b34ce6b21d3965d9c159eab3d9
Omri Rozenzaft <omrirz@gmail.com>
2022-03-21 22:38:00 +0200

    StateListener on init

--- nutshell-frontend/src/pages/Popup/lib/state-listener.tsx
+++ nutshell-frontend/src/pages/Popup/lib/state-listener.tsx
@@ -1,18 +1,27 @@
+import { useEffect } from 'react';
 import { useSetRecoilState } from 'recoil';
 import { openClosedAtom } from './atoms';
 
 export default function StateListener() {
   const setOpenClosed = useSetRecoilState(openClosedAtom);
-
-  chrome.storage.onChanged.addListener(function (changes, namespace) {
-    if ('SHOW_APP_ON_INIT' in changes) {
-      setOpenClosed(changes.SHOW_APP_ON_INIT.newValue);
+  useEffect(() => {
+    chrome.storage.onChanged.addListener(function (changes, namespace) {
+      if ('SHOW_APP_ON_INIT' in changes) {
+        setOpenClosed(changes.SHOW_APP_ON_INIT.newValue);
+        console.debug(
+          '[@@@@ StateListener] show:',
+          changes.SHOW_APP_ON_INIT.newValue
+        );
+      }
+    });
+    chrome.storage.sync.get(['SHOW_APP_ON_INIT'], (items) => {
+      setOpenClosed(items.SHOW_APP_ON_INIT);
       console.debug(
-        '[@@@@ StateListener] show:',
-        changes.SHOW_APP_ON_INIT.newValue
+        '[@@@@ StateListener] show on init:',
+        items.SHOW_APP_ON_INIT
       );
-    }
-  });
+    });
+  }, []);
 
   return null;
 }

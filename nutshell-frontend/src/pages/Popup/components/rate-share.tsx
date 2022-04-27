59866a74a18243c59fc17e3d854e233ec5ad751b
Nati Cohen <naticohenn@gmail.com>
2022-04-28 01:19:14 +0300

    like / dislike can't be 'on' together (they should toggle each otehr)

--- nutshell-frontend/src/pages/Popup/components/rate-share.tsx
+++ nutshell-frontend/src/pages/Popup/components/rate-share.tsx
@@ -2,7 +2,7 @@ import { default as React } from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ReactTooltip from 'react-tooltip';
-import { useRecoilValue } from 'recoil';
+import { atom, useRecoilState, useRecoilValue } from 'recoil';
 import { LS_PREFIX, summaryState } from '../lib/atoms';
 import { DATA_LOADING } from '../lib/data-bus';
 import { useEventLogger, UserEvent } from '../lib/event-logger';
@@ -11,6 +11,10 @@ import { IconButton } from './button';
 import { ShareIcon, ThubsDownIcon, ThubsUpIcon } from './icons';
 import { useShare, useShareKey } from './share';
 
+const toggleButtonState = atom({
+  key: 'toggleButtonState',
+  default: '',
+});
 export default function RateShareSection() {
   return (
     <div className="mt-4 grid grid-cols-1fr-3-auto gap-x-4 items-center mb-4">
@@ -25,21 +29,24 @@ export default function RateShareSection() {
 }
 
 const ThumbsUp = () => {
+  const [toggleButton, setToggleButton] = useRecoilState(toggleButtonState);
   const url = localStorage.getItem(LS_PREFIX);
   const [clicked, setClicked] = useLocalStorage(`${url}__clicked_like`, false);
   const { eventLogger } = useEventLogger();
+
   return (
     <>
       <IconButton
         onClick={() => {
           eventLogger(UserEvent.CLICKED_LIKE);
           setClicked(!clicked);
+          setToggleButton('like');
         }}
         dataTip={'Like'}
         dataFor={'controls-like'}
         ariaLabel={'Like'}
         className={`!bg-darkGray !rounded-md !p-3 !w-16 !h-16 text-blue hover:text-cyan duration-300 ${
-          clicked ? '!text-white !bg-blue' : ''
+          toggleButton === 'like' ? '!text-white !bg-blue' : ''
         }`}
       >
         <ThubsUpIcon />
@@ -50,6 +57,7 @@ const ThumbsUp = () => {
 };
 
 const ThumbsDown = () => {
+  const [toggleButton, setToggleButton] = useRecoilState(toggleButtonState);
   const { eventLogger } = useEventLogger();
   const url = localStorage.getItem(LS_PREFIX);
   const [clicked, setClicked] = useLocalStorage(
@@ -62,12 +70,13 @@ const ThumbsDown = () => {
         onClick={() => {
           eventLogger(UserEvent.CLICKED_DISLIKE);
           setClicked(!clicked);
+          setToggleButton('dislike')
         }}
         dataTip={'Dislike'}
         dataFor={'controls-dislike'}
         ariaLabel={'Dislike'}
         className={`!bg-darkGray !rounded-md !p-3 !w-16 !h-16 text-blue hover:text-cyan duration-300 ${
-          clicked ? '!text-white !bg-blue' : ''
+          toggleButton === 'dislike' ? '!text-white !bg-blue' : ''
         }`}
       >
         <ThubsDownIcon />

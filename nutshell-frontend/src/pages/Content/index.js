93c7a70f06a9f65f561fb760e1f8358020c8dd7b
Nati Cohen <naticohenn@gmail.com>
2022-04-28 02:30:39 +0300

    fix href

--- nutshell-frontend/src/pages/Content/index.js
+++ nutshell-frontend/src/pages/Content/index.js
@@ -50,7 +50,7 @@ function toggle() {
   if (root?.style?.transform === 'translateX(0px)') {
     hide();
     return false;
-  } else if(window.location.href.includes("oneai.com")){
+  } else if(window.location.href.includes("oneai.com/nutshell-share/")){
     hide();
 
   } else {
